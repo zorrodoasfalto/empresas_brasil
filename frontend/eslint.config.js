865e6c756a493a43284586631fc0c067c66cefaa
Leandro <leandro@leandroo.com.br>
2025-09-24 17:35:41 -0300

    Fix Express wildcard route for production build

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,3 @@
-import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -9,7 +8,6 @@ export default defineConfig([
   {
     files: ['**/*.{js,jsx}'],
     extends: [
-      js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
